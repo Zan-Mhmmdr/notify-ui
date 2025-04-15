bd52470d3bc20f6d5bac7c3b38521e5de9d7a001
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-04-15 16:37:51 +0700

    feat: add dynamic notification rendering

---
+++ script.js
@@ -0,0 +1,23 @@
+fetch('notifications.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('content');
+
+    data.forEach(notif => {
+      const div = document.createElement('div');
+      div.className = 'content';
+      div.innerHTML = `
+        <img src="${notif.avatar}" alt="${notif.username}'s avatar">
+        <div class="title-content">
+          <p>
+            <a href="#" class="username">${notif.username}</a>
+            ${notif.action}
+            ${notif.target ? `<a href="#" class="desc desc-text"> ${notif.target}</a>` : ""}
+          </p>
+          <span class="timestamp">${notif.time}</span>
+          ${notif.message ? `<div class="message">${notif.message}</div>` : ""}
+        </div>
+      `;
+      container.appendChild(div);
+    });
+  });
